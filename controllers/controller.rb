0b5686b5ba12d02120a275be2a13ee494b504c5d
Steph Vallance <44055497+steph603@users.noreply.github.com>
2020-10-01 12:01:18 +1000

    Refactor search for display & checkin/out

--- controllers/controller.rb
+++ controllers/controller.rb
@@ -6,20 +6,39 @@ require_relative '../views/displaycat'
 require_relative '../views/meals'
 
 module Controller
+    
 
     def self.search
-        puts "hi!"
-        ## Put TTY Prompts in here
-        prompt = TTY::Prompt.new
+#Allows user to search for a cat by ID number or by name
+        all_cats = Guests.all
 
-        prompt.ask('Please enter the cat\'s name, or ID# if you know it')
+        prompt = TTY::Prompt.new(active_color: :magenta).ask('Please enter the cat\'s name, or ID# if you know it'.bold)
+# If user knows the ID number, the cat is returned immediately
+        if prompt == prompt.to_i.to_s
+             cat = all_cats.select{|cat| cat[:id] == prompt.to_i}
 
-        if prompt  
-
+            else
+                results = all_cats.select{|cat| cat[:name].include? prompt.chomp.strip.capitalize}
+# If the user would like to search by name, follow the below prompts
+                if results.length == 0
+                    puts "\nNo results found".red.bold
+                elsif results.length == 1
+                    cat = results
+                elsif results.length > 1
+                    puts 'Here are the results!'
+                        id_table = TTY::Table.new(header: ['ID #'.bold, 'Name'.bold, 'Colour'.bold, 'Breed'.bold])
+                        results.each {|result| id_table << [result[:id], result[:name], result[:colour], result[:breed]]}
+                    puts id_table.render(:unicode, padding: [1,1,1,1])
+                        prompt = TTY::Prompt.new(active_color: :magenta).ask('Which is the correct ID#?'.bold){|q| q.in("0-99999")}
+                        cat = all_cats.select{|cat| cat[:id] == prompt.to_i}
+        end
     end
+ end
 
 
-    # Directs to show either all cats, or all checked in cats
+
+
+# Directs to show either all cats, or all checked in cats
     def self.index(which)
         all_cats = Guests.all
             if which == 4
@@ -30,25 +49,23 @@ module Controller
         ::Views::Guests::index(cats: cats)
     end 
 
-    # Shows prompts to switches checking in/out and processes cat accordingly
+# Shows prompts to switches checking in/out and processes cat accordingly
     def self.check_in_out(inout)
-        search()
-        all_cats = Guests.all
-        inout.to_i == 1 ? (inout = 'in') : (inout = 'out')
-        ::Views::Guests::check(inout: inout, all_cats: all_cats)
+        cat = search()
+        inout == 1 ? (inout = 'in') : (inout = 'out')
+        ::Views::Guests::check(inout: inout, cat: cat)
     end
 
-# Shows promptes to create a new cat
+# Shows prompts to create a new cat
     def self.new_cat
         all_cats = Guests.all
         ::Views::Guests::create(all_cats: all_cats)
     end
 
-    # Shows an individual cat  
-    #ONLY USE ID
+
     def self.displays
-        all_cats = Guests.all
-        ::Views::Guests::show_cat(all_cats: all_cats)
+        cat = search()
+        ::Views::Guests::show_cat(cat: cat)
     end
 
     def self.show_meals
--- views/displaycat.rb
+++ views/displaycat.rb
@@ -3,37 +3,8 @@ require 'tty-table'
 
 module Views
     module Guests
-        def self.show_cat(all_cats:)
-
-            puts "Please enter cat's name:"
-            
-            search = gets.chomp.strip.capitalize
-            results = all_cats.select{|cat| cat[:name].include? search}
-            
-            if results.length == 0
-                puts "\nNo results found".red.bold
-
-            elsif results.length == 1
-                results = results[0]
-                cat_card = TTY::Table.new(header: [results[:name], results[:colour], results[:breed]]) 
-                cat_card << [results[:am], results[:pm], results[:spicy]]
-                puts cat_card.render(:unicode, padding: [1,1,1,1])
-
-            elsif results.length > 1
-
-                puts 'Here are the results!'
-                id_table = TTY::Table.new(header: ['ID #'.bold, 'Name'.bold, 'Colour'.bold, 'Breed'.bold])
-                results.each {|result| id_table << [result[:id], result[:name], result[:colour], result[:breed]]}
-                puts id_table.render(:unicode, padding: [1,1,1,1])
-
-            puts 'Please enter the ID number of the cat you need.'
-
-            id = gets.chomp.strip.to_i
-            result = results.select{|result| [result[:id]].include? id}
-puts "YOU NEED TO MAKE A TABLE".red.bold
-            puts result
-
-            end
+        def self.show_cat(cat:)
+            puts cat
         end
 
     end
